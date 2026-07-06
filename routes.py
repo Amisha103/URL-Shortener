df788c4361eba2881f841536f28c7505712c9625
Amisha103 <bhatiaamisha74@gmail.com>
2026-07-06 12:36:51 +0530

    GET method Implemented

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, jsonify
-from services import create_short_url
+from flask import Blueprint, request, jsonify, redirect
+from services import create_short_url, get_original_url
 
 url_bp = Blueprint("url", __name__)
 
@@ -19,3 +19,15 @@ def shorten_url():
         "original_url": url.original_url,
         "short_code": url.short_code
     }), 201
+
+@url_bp.route("/<short_code>", methods=["GET"])
+def redirect_url(short_code):
+
+    url = get_original_url(short_code)
+
+    if not url:
+        return jsonify({
+            "message": "Short URL not found"
+        }), 404
+
+    return redirect(url.original_url)
